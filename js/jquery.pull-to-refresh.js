6cae5c393e53bcaeb6c032eb67d170c64ca11f17
Sander van de Graaf <mail@svdgraaf.nl>
2011-10-26 13:11:38 +0200

    added working javascript, added css markup for demo

--- js/jquery.pull-to-refresh.js
+++ js/jquery.pull-to-refresh.js
@@ -1,73 +1,98 @@
-var PULL = function() {
-  var content,
-      status_container,
-      refreshing,
-      contentStartY,
-      success, 
-      start, 
-      cancel,
-      startY,
-      track = false,
-      refresh = false;
+(function($){
+  $.fn.pull_to_refresh = function(options) {
 
-  var removeTransition = function() {
-    content.style['-webkit-transition-duration'] = 0;
-  };
+    var defaults = {
+      refresh: function(){},
+      start: function(){},
+      cancel: function(){},
+      pull_to_refresh_text: 'Pull down to refresh...',
+      refreshing_text: 'Refreshing...',
+      status_indicator_id: 'pull_to_refresh',
+      refreshClass: 'refresh',
+    };
+    var options = $.extend(defaults, options);
 
-  return {
-    init: function(o) {
-      content = document.getElementById('content');
-      pullToRefresh = document.getElementById('pull_to_refresh');
-      refreshing = document.getElementById('refreshing');
-      success = o.success;
-       start = o.start;
-      cancel = o.cancel;
+    var content,
+        status_indicator,
+        refreshing,
+        contentStartY = 0,
+        startY = 0,
+        track = false,
+        refresh = false;
 
-      document.body.addEventListener('touchstart', function(e) {
-        e.preventDefault();
-        contentStartY = parseInt(content.style.top);
-        startY = e.touches[0].screenY;
-      });
+    var removeTransition = function() {
+      content.css('-webkit-transition-duration',0);
+      status_indicator.removeClass(o.refreshClass);
+    };
 
-      document.body.addEventListener('touchend', function(e) {
-        if(refresh) {
-          content.style['-webkit-transition-duration'] = '.5s';
-          content.style.top = '50px';
-          
-          pullToRefresh.style.display = 'none';
-          refreshing.style.display = '';
+    // get the different objects
+    content = $(this);
 
-          success(function() { // pass down done callback
-            pullToRefresh.style.display = '';
-            refreshing.style.display = 'none';
-            content.style.top = '0';
-            content.addEventListener('transitionEnd', removeTransition);
-          });
+    // create the container for the pull indicator
+    content.prepend('<div id="' + options.status_indicator_id + '">' + options.pull_to_refresh_text + '</div>');
+    status_indicator = $('#' + options.status_indicator_id);
 
-          refresh = false;
-        } else if(track) {
-          content.style['-webkit-transition-duration'] = '.25s';
-          content.style.top = '0';
+    // make sure the content is absolute positioned
+    content.css('position','absolute');
+    content.css('top',0);
+
+    // move the pull_to_refresh to the top
+    status_indicator.css('position','absolute');
+    status_indicator.css('top','-' + status_indicator.height() + 'px');
+    status_indicator.css('display','block');
+
+    // add the listener for the touchdown event, and set the initial values
+    document.body.addEventListener('touchstart', function(e) {
+      try {
+        contentStartY = parseInt(content.css('top'));
+      }
+      catch(e) {
+        contentStartY = 0;
+      }
+      startY = e.touches[0].screenY;
+    });
+
+    // this is where the magic happens, if we stop moving, we check
+    // whether we haved moved enough, if so, we set a nice transition, and 
+    // start the callback
+    document.body.addEventListener('touchend', function(e) {
+      if(refresh) {
+        content.css('-webkit-transition-duration','.5s');
+        content.css('top',status_indicator.height() + 'px');
+        status_indicator.text(options.refreshing_text);
+        status_indicator.addClass(o.refreshClass);
+
+        options.refresh(function() { // pass down done callback
           content.addEventListener('transitionEnd', removeTransition);
+        });
 
-          cancel();
-        }
+        refresh = false;
+      } else if(track) {
+        content.css('-webkit-transition-duration','.25s');
+        content.css('top','0');
+        content.addEventListener('transitionEnd', removeTransition);
+        cancel();
+      }
 
-        track = false;
-      });
+      track = false;
+    });
 
-      document.body.addEventListener('touchmove', function(e) {
-        var move_to = contentStartY - (startY - e.changedTouches[0].screenY);
-        if(move_to > 0) track = true; // start tracking if near the top 
-        content.style.top = move_to + 'px';
+    // on moving of the touch event, we move the diff down
+    // unfortunately, for this to work, we need to prevent the default
+    // behaviour, which means a broken bounce, any tips would be appreciated
+    document.body.addEventListener('touchmove', function(e) {
+      e.preventDefault(); // <- bummer!
+      var move_to = contentStartY - (startY - e.changedTouches[0].screenY);
+      if(move_to > 0) track = true; // start tracking if near the top 
+      content.css('top',move_to + 'px');
 
-        if(move_to > 50) {
-          refresh = true;
-        } else {
-          content.style['-webkit-transition'] = '';
-          refresh = false;
-        }
-      });
-    }
-  };
-}();
+      // have we pull the whole indicator down?
+      if(move_to > status_indicator.height()) {
+        refresh = true;
+      } else {
+        content.css('-webkit-transition','');
+        refresh = false;
+      }
+    });
+  }
+})(jQuery);
